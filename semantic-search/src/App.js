ef17b83cfe76e5b1c5254ee6a7d11215fc6f90d6
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2023-04-07 15:11:47 -0500

    Create beginning structure, install react app

---
+++ semantic-search/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Yeap
+        </a>
+    </div>
+  );
+}
+
+export default App;
