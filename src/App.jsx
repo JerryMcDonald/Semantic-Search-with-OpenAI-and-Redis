359dabf79c3e835cd08aff033561d3e06d41fc87
Anish Gyawali <anish.gyawali@dxc.com>
2023-04-26 11:54:42 -0500

    updaed import path for styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// import './styles.css';
+import './styles/navigation.css';
 import Dashboard from './pages/Dashboard/Dashboard.jsx';
 import Admin from './pages/Admin/Admin.jsx';
 import Navigation from './components/Navigation/Navigation.jsx';
